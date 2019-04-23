5ebf99b8cd1a4cd37b1c0471e2f468dd8f5b6ba1
Adam Gashlin <agashlin@gmail.com>
2019-04-22 22:58:39 -0700

    Fix ChunkIter

--- src/chunk.rs
+++ src/chunk.rs
@@ -28,20 +28,22 @@ impl<Iter: Iterator<Item=Separator>> Iterator for ChunkIter<Iter> {
     fn next(&mut self) -> Option<Self::Item> {
         match self.separators.next() {
             Some(separator) => {
+                let chunk_index = self.last_separator_index;
                 let chunk_size = separator.index - self.last_separator_index;
                 self.last_separator_index = separator.index;
                 return Some(Chunk {
-                    index: self.last_separator_index,
+                    index: chunk_index,
                     size: chunk_size,
                     separator_hash: separator.hash,
                 });
             },
             None => {
+                let chunk_index = self.last_separator_index;
                 let chunk_size = self.stream_length - self.last_separator_index;
                 self.last_separator_index = self.stream_length;
                 if chunk_size > 0 {
                     return Some(Chunk {
-                        index: self.last_separator_index,
+                        index: chunk_index,
                         size: chunk_size,
                         separator_hash: 0, // any value is ok, last chunk of the stream.
                     });
@@ -51,5 +53,35 @@ impl<Iter: Iterator<Item=Separator>> Iterator for ChunkIter<Iter> {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{ChunkIter, SeparatorIter};
+    #[test]
+    fn test_whole() {
+        let bytes: Vec<u8> = (0..4096u32).map(|b| b as u8).collect();
+
+        let chunks: Vec<_> = ChunkIter::new(
+            SeparatorIter::custom_new(
+                bytes.iter().cloned(),
+                4,
+                |hash| hash & 0xff == 0,
+            ),
+            bytes.len() as u64,
+        ).collect();
+
+        assert_eq!(chunks[0].index, 0);
+
+        let mut i = 0;
+        for chunk in &chunks {
+            assert_eq!(chunk.index, i);
+            i += chunk.size;
+        }
+
+        let last_chunk = &chunks[chunks.len()-1];
+        assert_eq!((last_chunk.index + last_chunk.size) as usize, bytes.len());
+
     }
 }
